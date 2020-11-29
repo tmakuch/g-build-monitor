983967a5e3a955bc4399d681bfcb8cec1aae1dd4
Tadeusz Makuch <tadeusz.makuch@gmail.com>
2020-11-29 14:52:34 +0100

    commit because I want to pick it back up from another computer

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const http = require("http");
-const express = require("express");
-
-const app = express();
+const app = require("express")();
+const bodyParser = require("body-parser");
 const port = process.env.APP_PORT || 90;
 
+app.use(bodyParser.json());
 app.use("/github", require("./src/github"));
-
 app.get("/health", (req, res) => res.send("Staying alive!").end());
 app.get("/", (req, res) => res.send("Hello world").end());
 
