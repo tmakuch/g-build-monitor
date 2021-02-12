f4d9751f9c080b676b3945d32487b313cb415695
Tadeusz Makuch <tadeusz.makuch@gmail.com>
2021-02-12 02:16:41 +0100

    Working partials, dropped tunnel.js for ngrok

--- Dockerfile.app
+++
@@ -1,5 +0,0 @@
-FROM node:14-alpine
-WORKDIR /usr/local/srv
-
-COPY . .
-RUN npm i
--- src/index.js
+++ src/index.js
@@ -1,22 +1,45 @@
 const config = require("../config");
 const plugins = require("./monitor/monitor");
+const { Subject } = require("rxjs");
 
 module.exports = (fastify, opts, done) => {
   fastify.get("/health", () => "ok");
 
-  const monitorData = plugins.init(fastify, config.monitorConfig, null);
+  const webhookStream = new Subject();
+  fastify.post("/webhooks", (req) => {
+    webhookStream.next({
+      headers: req.headers,
+      body: req.body,
+    });
+  });
+
+  const monitorData = plugins.init(
+    fastify,
+    config.monitorConfig,
+    webhookStream,
+  );
 
   fastify.io.of("/").on("connection", (socket) => {
     fastify.log.info("Socket connected: " + socket.id);
 
-    const sub = monitorData.getStream().subscribe((tick) => {
-      fastify.log.info(`Ticking for ${socket.id}: ${JSON.stringify(tick)}`);
-      socket.emit("tick", tick);
+    const fullSub = monitorData.getFullStream().subscribe((full) => {
+      fastify.log.info(
+        `Sending full info for ${socket.id}: ${JSON.stringify(full)}`,
+      );
+      socket.emit("full", full);
+    });
+
+    const partialSub = monitorData.getPartialStream().subscribe((partial) => {
+      fastify.log.info(
+        `Sending partial info for ${socket.id}: ${JSON.stringify(partial)}`,
+      );
+      socket.emit("partial", partial);
     });
 
     socket.on("disconnect", () => {
       fastify.log.info("Socket disconnected: " + socket.id);
-      sub.unsubscribe();
+      fullSub.unsubscribe();
+      partialSub.unsubscribe();
     });
   });
 
